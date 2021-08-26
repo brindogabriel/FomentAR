3f1b4862130d7de1ae527f47fe1fe1213daa53c2
Gabriel Brindo <gabrielbrindo18@gmail.com>
2021-08-26 17:04:25 -0300

    arreglando datatables xd

--- scripts.php
+++ scripts.php
@@ -59,43 +59,9 @@ function myFunction() {
 }
 </script>
 
-
-
-<script src="../CosasParaQueFuncioneSinInternet/jquery-3.3.1.js"></script>
-<script src="./popper.min.js"></script>
-<link rel="stylesheet" type="text/css"
-    href="../CosasParaQueFuncioneSinInternet/DataTables/DataTables-1.10.18/css/dataTables.bootstrap4.css" />
-<link rel="stylesheet" type="text/css"
-    href="../CosasParaQueFuncioneSinInternet/DataTables/Buttons-1.5.4/css/buttons.bootstrap4.css" />
-<link rel="stylesheet" type="text/css"
-    href="../CosasParaQueFuncioneSinInternet/DataTables/FixedColumns-3.2.5/css/fixedColumns.bootstrap4.css" />
-<link rel="stylesheet" type="text/css"
-    href="../CosasParaQueFuncioneSinInternet/DataTables/FixedHeader-3.1.4/css/fixedHeader.bootstrap4.css" />
-
-<script type="text/javascript" src="../CosasParaQueFuncioneSinInternet/DataTables/JSZip-2.5.0/jszip.js"></script>
-<script type="text/javascript" src="../CosasParaQueFuncioneSinInternet/DataTables/pdfmake-0.1.36/pdfmake.js"></script>
-<script type="text/javascript" src="../CosasParaQueFuncioneSinInternet/DataTables/pdfmake-0.1.36/vfs_fonts.js"></script>
-<script type="text/javascript"
-    src="../CosasParaQueFuncioneSinInternet/DataTables/DataTables-1.10.18/js/jquery.dataTables.js"></script>
-<script type="text/javascript"
-    src="../CosasParaQueFuncioneSinInternet/DataTables/DataTables-1.10.18/js/dataTables.bootstrap4.js"></script>
-<script type="text/javascript"
-    src="../CosasParaQueFuncioneSinInternet/DataTables/Buttons-1.5.4/js/dataTables.buttons.js"></script>
-<script type="text/javascript"
-    src="../CosasParaQueFuncioneSinInternet/DataTables/Buttons-1.5.4/js/buttons.bootstrap4.js"></script>
-<script type="text/javascript" src="../CosasParaQueFuncioneSinInternet/DataTables/Buttons-1.5.4/js/buttons.colVis.js">
-</script>
-<script type="text/javascript" src="../CosasParaQueFuncioneSinInternet/DataTables/Buttons-1.5.4/js/buttons.flash.js">
-</script>
-<script type="text/javascript" src="../CosasParaQueFuncioneSinInternet/DataTables/Buttons-1.5.4/js/buttons.html5.js">
-</script>
-<script type="text/javascript" src="../CosasParaQueFuncioneSinInternet/DataTables/Buttons-1.5.4/js/buttons.print.js">
-</script>
-<script type="text/javascript"
-    src="../CosasParaQueFuncioneSinInternet/DataTables/FixedColumns-3.2.5/js/dataTables.fixedColumns.js"></script>
-<script type="text/javascript" src="../CosasParaQueFuncioneSinInternet/FixedHeader-3.1.4/js/dataTables.fixedHeader.js">
-</script>
-
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<script src="https://cdn.datatables.net/1.11.0/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.11.0/js/dataTables.bootstrap4.min.js"></script>
 <script>
 $(document).ready(function() {
     $('#example').DataTable({
