9e538007bb91998b89dd2c5d62bfc7b4f6043f9f
Cristian Villavicencio <cris_13201@outlook.com>
2022-06-03 01:22:47 -0300

    aber

--- FomentAR/modificar5.php
+++ FomentAR/modificar5.php
@@ -33,45 +33,52 @@ function ConsultarCliente( $DNI )
 
 
 ?>
-<body>
-	<div class="contenedor p-1" style="">
-		<form action="modificar6.php" method="post">
-			<div class="form-group">
-				<input type="hidden" name="Usuario"  value="<?php echo $_GET['Nro_orden']?>">
-				<label for="nombre">Nombre</label>
-				<input type="text" class="form-control" placeholder="Nombre" name="Nombre" value="<?php echo $consulta[0] ?>" required>
-			</div>
-			<div class="form-group">
-				<label for="apellido">Apellido</label>
-				<input type="text" class="form-control" placeholder="Apellido" name="apellido" value="<?php echo utf8_encode($consulta[1]) ?>" required>
-			</div>
-			<div class="form-group">
-				<label for="domicilio">Domicilio</label>
-				<input type="text" class="form-control" placeholder="Domicilio" name="domicilio" value="<?php echo $consulta[3] ?>" required>
-			</div>
-			<div class="form-group">
-				<label for="user">DNI</label>
-				<input type="number" class="form-control" placeholder="DNI" name="DNI" id="cantidad"  value="<?php echo $consulta[4] ?>" required>
-			</div>
-			<div class="form-group">
-				<label for="fecha_nacimiento">Fecha de nacimiento</label>
-				<input type="date" name="fecha_nacimiento" max="3000-12-31" 
-				min="1000-01-01" class="form-control" placeholder="Fecha de nacimiento" name="fecha_nacimiento" value="<?php echo $consulta[5] ?>"  required >
-			</div>
-			<div class="form-group">
-				<label for="fecha_ingreso">Fecha de ingreso</label>
-				<input type="date" name="fecha_ingreso" max="3000-12-31" 
-				min="1000-01-01" class="form-control" placeholder="Fecha de ingreso" name="fecha_ingreso" value="<?php echo $consulta[6] ?>" required>
-			</div>
-			<div class="form-group">
-				<label for="exampleFormControlSelect">¿Es Socio?</label>
-				<select simple class="form-control" id="exampleFormControlSelect" name="socio" required value="<?php echo $consulta[7] ?>">
-					<option value="1">Si</option>
-					<option value="2">No</option>
 
-				</select>
-			</div>
-<!-- 			<div class="form-group">
+<body>
+    <div class="contenedor p-1" style="">
+        <form action="modificar6.php" method="post">
+            <div class="form-group">
+                <input type="hidden" name="Usuario" value="<?php echo $_GET['nro_orden']?>">
+                <label for="nombre">Nombre</label>
+                <input type="text" class="form-control" placeholder="Nombre" name="nombre"
+                    value="<?php echo $consulta[0] ?>" required>
+            </div>
+            <div class="form-group">
+                <label for="apellido">Apellido</label>
+                <input type="text" class="form-control" placeholder="Apellido" name="apellido"
+                    value="<?php echo utf8_encode($consulta[1]) ?>" required>
+            </div>
+            <div class="form-group">
+                <label for="domicilio">Domicilio</label>
+                <input type="text" class="form-control" placeholder="Domicilio" name="domicilio"
+                    value="<?php echo $consulta[3] ?>" required>
+            </div>
+            <div class="form-group">
+                <label for="user">DNI</label>
+                <input type="number" class="form-control" placeholder="DNI" name="DNI" id="cantidad"
+                    value="<?php echo $consulta[4] ?>" required>
+            </div>
+            <div class="form-group">
+                <label for="fecha_nacimiento">Fecha de nacimiento</label>
+                <input type="date" name="fecha_nacimiento" max="3000-12-31" min="1000-01-01" class="form-control"
+                    placeholder="Fecha de nacimiento" name="fecha_nacimiento" value="<?php echo $consulta[5] ?>"
+                    required>
+            </div>
+            <div class="form-group">
+                <label for="fecha_ingreso">Fecha de ingreso</label>
+                <input type="date" name="fecha_ingreso" max="3000-12-31" min="1000-01-01" class="form-control"
+                    placeholder="Fecha de ingreso" name="fecha_ingreso" value="<?php echo $consulta[6] ?>" required>
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlSelect">¿Es Socio?</label>
+                <select simple class="form-control" id="exampleFormControlSelect" name="socio" required
+                    value="<?php echo $consulta[7] ?>">
+                    <option value="1">Si</option>
+                    <option value="2">No</option>
+
+                </select>
+            </div>
+            <!-- 			<div class="form-group">
 				<label for="exampleFormControlSelect2">Actividad</label>
 				<select multiple class="form-control" id="exampleFormControlSelect2" name="" required>
 					<option value="basquet">Basquet</option>
@@ -82,17 +89,18 @@ function ConsultarCliente( $DNI )
 					<option value="voley">Voley</option>
 				</select>
 			</div> -->
-			<div class="dropdown-divider"></div>
-			<button type="button" class="btn btn-secondary float-right" onclick="history.back()">Cancelar</button>
-			<input type="submit" class="btn btn-primary" name="submit" value="Actualizar">
-		</form>
-	</div>
+            <div class="dropdown-divider"></div>
+            <button type="button" class="btn btn-secondary float-right" onclick="history.back()">Cancelar</button>
+            <input type="submit" class="btn btn-primary" name="submit" value="Actualizar">
+        </form>
+    </div>
 
-	<!-- Optional JavaScript -->
-	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js"></script>
-	<script src="script.js"></script>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js"></script>
+    <script src="script.js"></script>
 </body>
+
 </html>
--- FomentAR/modificar6.php
+++ FomentAR/modificar6.php
@@ -3,14 +3,14 @@
 
 
 
-ModificarCliente($_POST['Nombre'],$_POST['apellido'],$_POST['domicilio'],$_POST['DNI'],$_POST['fecha_nacimiento'],$_POST['fecha_ingreso'],$_POST['socio']);
+ModificarCliente($_POST['nombre'],$_POST['apellido'],$_POST['domicilio'],$_POST['DNI'],$_POST['fecha_nacimiento'],$_POST['fecha_ingreso'],$_POST['socio']);
 
 function ModificarCliente($nombre,$apellido,$domicilio,$dni,$fecha_nacimiento,$fecha_ingreso,$socio)
 {
 	include 'conexion.php';
 	$Edad = strtotime ($fecha_ingreso) - strtotime ($fecha_nacimiento);
 	$diferencia_anios = intval($Edad/60/60/24/365.25);
-	$sentencia="UPDATE `clientes` SET `Nombre` = '$nombre', `Apellido` = '$apellido', `Domicilio` = '$domicilio', `DNI` = '$dni', `Fecha_nacimiento` = '$fecha_nacimiento', `Fecha_ingreso` = '$fecha_ingreso', `idParametro_Socio` = '$socio'  WHERE DNI = $dni";
+	$sentencia="UPDATE `clientes` SET `nombre` = '$nombre', `Apellido` = '$apellido', `Domicilio` = '$domicilio', `DNI` = '$dni', `Fecha_nacimiento` = '$fecha_nacimiento', `Fecha_ingreso` = '$fecha_ingreso', `idParametro_Socio` = '$socio'  WHERE DNI = $dni";
 	$conexion->query($sentencia) or die ("Error al actualizar datos".mysqli_error($conexion));
 	echo "<script>history.go(-2);</script>"; 
 }
