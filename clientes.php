677a3d9f07adbdfa3087032bbace877f28698d8f
Gabriel Brindo <gabrielbrindo18@gmail.com>
2022-06-13 20:55:13 -0300

    FIX VIEW CLIENTES

--- clientes.php
+++ clientes.php
@@ -1,56 +1,138 @@
-<?php include("./nav_externo.php");?>
-<?php 
-include 'conexion.php';
+<?php
+session_start();
+//error_reporting(0); -descomentar cuando se termina
+$varsesion = $_SESSION['usuario'];
+if ($varsesion == null || $varsesion= '') {
+	header("location: ./errors/error_nologueado");
+	die();
+}
+$conexion=mysqli_connect("localhost","root","","fomentar");
+?>
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="./Resources/bootstrap-4.1.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/general.css">
+    <link rel="shortcut icon" href="./Images/logo.png" type="image/x-icon">
+    <title>FomentAR</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+        <a class="navbar-brand mb-0 h1" href="pagina_principal">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <path d="M0 0h24v24H0z" fill="none" />
+                <path d="M4 10v7h3v-7H4zm6 0v7h3v-7h-3zM2 22h19v-3H2v3zm14-12v7h3v-7h-3zm-4.5-9L2 6v2h19V6l-9.5-5z"
+                    fill="white" />
+            </svg>
+            FomentAR
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="./pagina_principal">Inicio<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class='nav-link' href='./clientes'>Todos los Clientes</a>
+                </li>
+                <!-- <li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Eventos
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<a class="dropdown-item" href="./eventos">Este mes</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" href="./historico">Historico</a>
+					</div>
+				</li>
+			-->
+                <!-- <li class="nav-item">
+					<?php 
+					$varsesion = $_SESSION['usuario'];
+					if ($varsesion == "presidente") {
+						echo "	<a class='nav-link' href='./recaudacion_total'>Recaudacion</a>";
+					}
+					?>							
+				</li> -->
+                <!-- <li class="nav-item">
+					<a class='nav-link' href='./reporte_errores'>Reporte Errores</a>
+				</li> -->
+                <li class="nav-item">
+                    <?php 
+					$varsesion = $_SESSION['usuario'];
+					if ($varsesion == "presidente") {
+						echo "	<a class='nav-link' href='./gestion_usuarios'>Gestion de usuarios</a>";
+					}
+					?>
+                </li>
+            </ul>
+            <a class="btn btn-primary disabled text-white mr-2" role="button" disabled
+                style="text-transform: capitalize;">
+                <?php
+				echo $varsesion;
+				?>
+            </a>
+            <a class="btn btn-outline-danger" href="./database/cerrar_sesion" role="button">Cerrar sesión</a>
+        </div>
+    </nav>
+    <?php 
+include './database/conexion.php';
 $clientes = "SELECT * FROM clientes";
 $resClientes=mysqli_query($conexion,$clientes);
 ?>
-<link rel="stylesheet" href="../CosasParaQueFuncioneSinInternet/material-icons.css">
 
-<meta charset="utf-8">
-<!-- <div class="botones">
+    <!-- <div class="botones">
 	<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
 		Agregar nuevo
 	</button>
 </div> -->
-<div class="contenedor">
-	<div class="table-responsive p-2" class="w-100">
-		<table id="example" class="table table-responsive display" width="100%" cellspacing="0">
-			<thead>
-				<tr>
-					<th>Nro_orden</th>
-					<th>Apellido</th>
-					<th>Nombre</th>
-					<th>Domicilio</th>
-					<th>DNI</th>
-					<th>Fecha_nacimiento</th>
-					<th>Fecha_ingreso</th>
-					<th>idParametro_Socio</th>
-					<th>idEstado</th>
-					<th>idCategoria</th>
-					<th>idSexo</th>
-					<th>Opciones</th>
+    <div class="contenedor">
+        <div class="table-responsive p-2" class="w-100">
+            <table id="example" class="table table-responsive display" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>Nro_orden</th>
+                        <th>Apellido</th>
+                        <th>Nombre</th>
+                        <th>Domicilio</th>
+                        <th>DNI</th>
+                        <th>Fecha_nacimiento</th>
+                        <th>Fecha_ingreso</th>
+                        <th>idParametro_Socio</th>
+                        <th>idEstado</th>
+                        <th>idCategoria</th>
+                        <th>idSexo</th>
+                        <th>Opciones</th>
 
-				</tr>
-			</thead>
-			<tfoot>
-				<tr>
-					<th>Nro_orden</th>
-					<th>Apellido</th>
-					<th>Nombre</th>
-					<th>Domicilio</th>
-					<th>DNI</th>
-					<th>Fecha_nacimiento</th>
-					<th>Fecha_ingreso</th>
-					<th>idParametro_Socio</th>
-					<th>idEstado</th>
-					<th>idCategoria</th>
-					<th>idSexo</th>
-					<th>Opciones</th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <th>Nro_orden</th>
+                        <th>Apellido</th>
+                        <th>Nombre</th>
+                        <th>Domicilio</th>
+                        <th>DNI</th>
+                        <th>Fecha_nacimiento</th>
+                        <th>Fecha_ingreso</th>
+                        <th>idParametro_Socio</th>
+                        <th>idEstado</th>
+                        <th>idCategoria</th>
+                        <th>idSexo</th>
+                        <th>Opciones</th>
 
-				</tr>
-			</tfoot>
-			<tbody>
-				<?php 
+                    </tr>
+                </tfoot>
+                <tbody>
+                    <?php 
 
 				while ($mostrar=mysqli_fetch_array($resClientes)) {
 					$dato = $mostrar['idParametro_Socio'];
@@ -80,12 +162,12 @@ $resClientes=mysqli_query($conexion,$clientes);
 					</tr>';
 				}
 				?>
-			</tbody>
-		</table>
-	</div>
-</div>
+                </tbody>
+            </table>
+        </div>
+    </div>
 
-<!-- <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <!-- <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
 	<div class="modal-dialog modal-dialog-centered" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
@@ -165,4 +247,12 @@ $resClientes=mysqli_query($conexion,$clientes);
 		</div>
 	</div>
 </div> -->
-<?php include 'scripts.php'; ?>
+    <?php include './scripts.php'; ?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="./js/jquery-3.3.1.slim.min.js"></script>
+    <script src="./js/popper.min.js"></script>
+    <script src="./Resources/bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>
