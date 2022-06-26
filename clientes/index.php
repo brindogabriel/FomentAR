70e484f5c8881463447889d0341783b3b02dc798
Gabriel Brindo <gabrielbrindo18@gmail.com>
2022-06-26 19:36:27 -0300

    xdd

--- clientes/index.php
+++ clientes/index.php
@@ -14,16 +14,16 @@ $conexion = mysqli_connect("localhost", "root", "", "fomentar");
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="./Resources/bootstrap-4.1.3-dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./css/general.css">
-    <link rel="shortcut icon" href="./Images/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="../Resources/bootstrap-4.1.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/general.css">
+    <link rel="shortcut icon" href="../Images/logo.png" type="image/x-icon">
     <title>FomentAR</title>
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
-        <a class="navbar-brand mb-0 h1" href="pagina_principal">
+        <a class="navbar-brand mb-0 h1" href="../pagina_principal">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                 <path d="M0 0h24v24H0z" fill="none" />
                 <path d="M4 10v7h3v-7H4zm6 0v7h3v-7h-3zM2 22h19v-3H2v3zm14-12v7h3v-7h-3zm-4.5-9L2 6v2h19V6l-9.5-5z"
@@ -38,10 +38,10 @@ $conexion = mysqli_connect("localhost", "root", "", "fomentar");
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="./pagina_principal">Inicio<span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="../pagina_principal">Inicio<span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class='nav-link' href='./clientes'>Todos los Clientes</a>
+                    <a class='nav-link' href='./'>Todos los Clientes</a>
                 </li>
                 <!-- <li class="nav-item dropdown">
 					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -58,7 +58,7 @@ $conexion = mysqli_connect("localhost", "root", "", "fomentar");
 					<?php
 					$varsesion = $_SESSION['usuario'];
 					if ($varsesion == "presidente") {
-						echo "	<a class='nav-link' href='./recaudacion_total'>Recaudacion</a>";
+						echo "	<a class='nav-link' href='../recaudacion_total'>Recaudacion</a>";
 					}
 					?>
 				</li> -->
@@ -69,7 +69,7 @@ $conexion = mysqli_connect("localhost", "root", "", "fomentar");
                     <?php
 					$varsesion = $_SESSION['usuario'];
 					if ($varsesion == "presidente") {
-						echo "	<a class='nav-link' href='./gestion_usuarios'>Gestion de usuarios</a>";
+						echo "	<a class='nav-link' href='../gestion_usuarios'>Gestion de usuarios</a>";
 					}
 					?>
                 </li>
@@ -84,7 +84,7 @@ $conexion = mysqli_connect("localhost", "root", "", "fomentar");
         </div>
     </nav>
     <?php
-	include './database/conexion.php';
+	include '../database/conexion.php';
 	$clientes = "SELECT * FROM clientes";
 	$resClientes = mysqli_query($conexion, $clientes);
 	?>
@@ -247,12 +247,12 @@ $conexion = mysqli_connect("localhost", "root", "", "fomentar");
 		</div>
 	</div>
 </div> -->
-    <?php include './scripts.php'; ?>
+    <script src="../js/jquery-3.3.1.slim.min.js"></script>
+    <script src="../js/popper.min.js"></script>
+    <script src="../Resources/bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
+    <?php include '../scripts.php'; ?>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="./js/jquery-3.3.1.slim.min.js"></script>
-    <script src="./js/popper.min.js"></script>
-    <script src="./Resources/bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
 </body>
 
 </html>
