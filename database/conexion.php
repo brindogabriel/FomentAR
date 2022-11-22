12928bc594001a1ce9e463e6317140f03401369f
Gabriel Brindo <gabrielbrindo18@gmail.com>
2022-11-21 23:59:07 -0300

    🐛 FIX: xddddd

--- database/conexion.php
+++ database/conexion.php
@@ -1,3 +1,3 @@
-<?php $conexion = mysqli_connect("localhost", "root", "", "fomentartesteo");
+<?php $conexion = mysqli_connect("localhost", "root", "", "fomentar");
 mysqli_query($conexion, "SET CHARACTER SET utf8");
 mysqli_query($conexion, "SET NAMES utf8");
