b228b261f0dde41d6b1c8379665aeecbb5587e58
Gabriel Brindo <gabrielbrindo18@gmail.com>
2022-11-28 01:11:26 -0300

    👌 IMPROVE: minor changes

--- database/conexion.php
+++ database/conexion.php
@@ -1,3 +1,4 @@
-<?php $conexion = mysqli_connect("localhost", "root", "", "primerfomentar");
+<?php
+$conexion = mysqli_connect("localhost", "root", "", "primerfomentar");
 mysqli_query($conexion, "SET CHARACTER SET utf8");
 mysqli_query($conexion, "SET NAMES utf8");
