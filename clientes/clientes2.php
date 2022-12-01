c4ac528ec1cbe7d97be9906b54ccf96d540644f5
Gabriel Brindo <gabrielbrindo18@gmail.com>
2022-12-01 15:32:37 -0300

    asd

--- clientes/clientes2.php
+++ clientes/clientes2.php
@@ -167,6 +167,7 @@ include "../database/conexion.php";
     <script src="../js/jquery-3.3.1.slim.min.js"></script>
     <script src="../js/popper.min.js"></script>
     <script src="../Resources/bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
+    <?php include "../scripts.php" ?>
 </body>
 
 </html>
