36f6a4a8afcf31ff9773cc0393ba377e51ea7505
Gabriel Brindo <gabrielbrindo18@gmail.com>
2022-12-04 18:20:11 -0300

    style: :art: xd

--- pagina_principal.php
+++ pagina_principal.php
@@ -51,23 +51,28 @@ include "./database/conexion.php";
                 <li class="nav-item">
                     <a class='nav-link' href='./eventos'>Eventos</a>
                 </li>
-                <li class="nav-item">
-                    <?php
-                    if ($rol == 1) {
-                        echo "<a class='nav-link' href='./recaudacion'>Recaudacion</a>";
-                    }
-                    ?>
-                </li>
+                <?php
+                if ($rol == 1) {
+                    echo "
+                <li class='nav-item'>                        
+                        <a class='nav-link' href='./recaudacion'>Recaudacion</a>
+                          </li>";
+                }
+                ?>
+
                 <li class="nav-item">
                     <a class='nav-link' href='./reporte_errores'>Reporte Errores</a>
                 </li>
-                <li class="nav-item">
-                    <?php
-                    if ($rol == 1) {
-                        echo "<a class='nav-link' href='./gestion_usuarios'>Gestion de usuarios</a>";
-                    }
-                    ?>
-                </li>
+
+                <?php
+                if ($rol == 1) {
+
+                    echo "<li class='nav-item'>
+                        <a class='nav-link' href='./gestion_usuarios'>Gestion de usuarios</a>
+                        </li>";
+                }
+                ?>
+
             </ul>
             <a class="btn btn-primary disabled text-white mr-2" role="button" disabled
                 style="text-transform: capitalize;">
@@ -81,55 +86,51 @@ include "./database/conexion.php";
     </nav>
     <div class="card-columns">
         <div class="card">
-            <a href="./futbol/"><img class="card-img-top " src="Images/futbol.jpg" alt="Card image cap"></a>
+            <a href="./clientes/clientesporactividad.php?id_actividad=2"><img class="card-img-top "
+                    src="Images/futbol.jpg" alt="Card image cap"></a>
             <div class="card-body text-center">
                 <p class="card-text">Fútbol</p>
                 <p class="card-text">
                     <?php
-                    $conexion = mysqli_connect("localhost", "root", "", "fomentar");
                     $sql = "SELECT COALESCE(COUNT(id_cliente), 0) as personas  from clientes_actividad where id_actividad = 2;";
-
                     $result = mysqli_query($conexion, $sql);
                     while ($mostrar = mysqli_fetch_assoc($result)) {
-
                         echo $mostrar['personas'];
                     }
                     ?> clientes
                 </p>
                 <div class="dropdown-divider"></div>
-                <a class="btn btn-secondary btn-block mt-3" href="./clientes/clientes2.php?idactividad=1"
+                <a class="btn btn-secondary btn-block mt-3" href="./clientes/clientesporactividad.php?id_actividad=2"
                     role="button">Ver Clientes</a>
             </div>
         </div>
         <div class="card">
-            <a href="./arte/"><img class="card-img-top " src="Images/arte.jpg" alt="Card image cap"></a>
+            <a href="./clientes/clientesporactividad.php?id_actividad=6"><img class="card-img-top "
+                    src="Images/arte.jpg" alt="Card image cap"></a>
             <div class="card-body text-center">
                 <p class="card-text">Arte</p>
                 <p class="card-text">
                     <?php
-                    $conexion = mysqli_connect("localhost", "root", "", "fomentar");
                     $sql = "SELECT COALESCE(COUNT(id_cliente), 0) as personas  from clientes_actividad where id_actividad = 6;";
-
                     $result = mysqli_query($conexion, $sql);
                     while ($mostrar = mysqli_fetch_assoc($result)) {
-
                         echo $mostrar['personas'];
                     }
                     ?> clientes
                 </p>
                 <div class="dropdown-divider"></div>
-                <a class="btn btn-secondary btn-block mt-3" href="./arte/" role="button">Ver Clientes</a>
+                <a class="btn btn-secondary btn-block mt-3" href="./clientes/clientesporactividad.php?id_actividad=6"
+                    role="button">Ver Clientes</a>
             </div>
         </div>
         <div class="card">
-            <a href="./basquet/"><img class="card-img-top " src="Images/basquet.jpg" alt="Card image cap"></a>
+            <a href="./clientes/clientesporactividad.php?id_actividad=1"><img class="card-img-top "
+                    src="Images/basquet.jpg" alt="Card image cap"></a>
             <div class="card-body text-center">
                 <p class="card-text">Basquet</p>
                 <p class="card-text">
                     <?php
-                    $conexion = mysqli_connect("localhost", "root", "", "fomentar");
                     $sql = "SELECT COALESCE(COUNT(id_cliente), 0) as personas  from clientes_actividad where id_actividad = 1;";
-
                     $result = mysqli_query($conexion, $sql);
                     while ($mostrar = mysqli_fetch_assoc($result)) {
 
@@ -138,18 +139,18 @@ include "./database/conexion.php";
                     ?> clientes
                 </p>
                 <div class="dropdown-divider"></div>
-                <a class="btn btn-secondary btn-block mt-3" href="./basquet/index.php" role="button">Ver Clientes</a>
+                <a class="btn btn-secondary btn-block mt-3" href="./clientes/clientesporactividad.php?id_actividad=1"
+                    role="button">Ver Clientes</a>
             </div>
         </div>
         <div class="card">
-            <a href="./taekwondo"><img class="card-img-top " src="Images/taekwondo.jpg" alt="Card image cap"></a>
+            <a href="./clientes/clientesporactividad.php?id_actividad=5"><img class="card-img-top "
+                    src="Images/taekwondo.jpg" alt="Card image cap"></a>
             <div class="card-body text-center">
                 <p class="card-text">Taekwondo</p>
                 <p class="card-text">
                     <?php
-                    $conexion = mysqli_connect("localhost", "root", "", "fomentar");
                     $sql = "SELECT COALESCE(COUNT(id_cliente), 0) as personas  from clientes_actividad where id_actividad = 5;";
-
                     $result = mysqli_query($conexion, $sql);
                     while ($mostrar = mysqli_fetch_assoc($result)) {
 
@@ -158,16 +159,17 @@ include "./database/conexion.php";
                     ?> clientes
                 </p>
                 <div class="dropdown-divider"></div>
-                <a class="btn btn-secondary btn-block mt-3" href="./taekwondo" role="button">Ver Clientes</a>
+                <a class="btn btn-secondary btn-block mt-3" href="./clientes/clientesporactividad.php?id_actividad=5"
+                    role="button">Ver Clientes</a>
             </div>
         </div>
         <div class="card">
-            <a href="./voley/"><img class="card-img-top " src="Images/voley.jpg" alt="Card image cap"></a>
+            <a href="./clientes/clientesporactividad.php?id_actividad=3"><img class="card-img-top "
+                    src="Images/voley.jpg" alt="Card image cap"></a>
             <div class="card-body text-center">
                 <p class="card-text">Voley</p>
                 <p class="card-text">
                     <?php
-                    $conexion = mysqli_connect("localhost", "root", "", "fomentar");
                     $sql = "SELECT COALESCE(COUNT(id_cliente), 0) as personas  from clientes_actividad where id_actividad = 3;";
 
                     $result = mysqli_query($conexion, $sql);
@@ -178,16 +180,17 @@ include "./database/conexion.php";
                     ?> clientes
                 </p>
                 <div class="dropdown-divider"></div>
-                <a class="btn btn-secondary btn-block mt-3" href="./voley/" role="button">Ver Clientes</a>
+                <a class="btn btn-secondary btn-block mt-3" href="./clientes/clientesporactividad.php?id_actividad=3"
+                    role="button">Ver Clientes</a>
             </div>
         </div>
         <div class="card">
-            <a href="./patin"><img class="card-img-top " src="Images/patin.jpg" alt="Card image cap"></a>
+            <a href="./clientes/clientesporactividad.php?id_actividad=4"><img class="card-img-top "
+                    src="Images/patin.jpg" alt="Card image cap"></a>
             <div class="card-body text-center">
                 <p class="card-text">Patín</p>
                 <p class="card-text">
                     <?php
-                    $conexion = mysqli_connect("localhost", "root", "", "fomentar");
                     $sql = "SELECT COALESCE(COUNT(id_cliente), 0) as personas  from clientes_actividad where id_actividad = 4;";
 
                     $result = mysqli_query($conexion, $sql);
@@ -198,7 +201,8 @@ include "./database/conexion.php";
                     ?> clientes
                 </p>
                 <div class="dropdown-divider"></div>
-                <a class="btn btn-secondary btn-block mt-3" href="./patin" role="button">Ver Clientes</a>
+                <a class="btn btn-secondary btn-block mt-3" href="./clientes/clientesporactividad.php?id_actividad=4"
+                    role="button">Ver Clientes</a>
             </div>
         </div>
     </div>
