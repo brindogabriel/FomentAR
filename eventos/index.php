bf3189fd288b343a45b311639e5f7690609832d9
Gabriel Brindo <gabrielbrindo18@gmail.com>
2022-12-26 01:08:10 -0300

    ASD

--- eventos/index.php
+++ eventos/index.php
@@ -191,15 +191,12 @@ include '..\database\conexion.php';
             </div>
         </div>
     </div>
-
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@3.10.5/dist/fullcalendar.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.0/main.min.js"></script>
-    <script src="./es.js"></script>
-    <script src="./script.js"></script>
+    <script src="script.js"></script>
     <script>
     var scheds = $.parseJSON('<?= json_encode($sched_res) ?>')
     </script>
