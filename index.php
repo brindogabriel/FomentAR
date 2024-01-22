a60bc15c691c2b3b3abfce29ef837e1139a58c2b
Gabriel Brindo <gabrielbrindo18@gmail.com>
2024-01-22 14:00:29 -0300

    feat: ✨ Add white shortcut icon in darkmode in index page only

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ if (isset($_SESSION['usuario'])) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="shortcut icon" href="./Images/logo.png" />
+  <link rel="icon" type="image/png" href="Images/logo-negro.png">
+   <link rel="icon" type="image/png" href="Images/logo-blanco.png" media="(prefers-color-scheme:dark)"> 
     <link rel="stylesheet" href="./Resources/bootstrap-4.1.3-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="./css/general.css">
     <!-- Primary Meta Tags -->
